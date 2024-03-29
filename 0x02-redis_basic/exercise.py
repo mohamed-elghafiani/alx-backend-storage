2f839605d9808e4275b8ed996426566898611c1b
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-29 16:49:33 -0700

    fixed naming issue

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,7 +31,7 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
-def reply(fn: Callable) -> None:
+def replay(fn: Callable) -> None:
     """display the history of calls of @fn"""
     r = redis.Redis()
     key = fn.__qualname__
